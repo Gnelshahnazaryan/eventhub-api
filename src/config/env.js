0b2527af8bd5442fdede56f45d6bd958186296f6
Gnel <gshahnazaryan08@gmail.com>
2026-07-04 00:24:49 +0400

    refactor: update env configuration

--- src/config/env.js
+++ src/config/env.js
@@ -2,9 +2,12 @@ require("dotenv").config({ quiet: true });
 
 const ENV = {
     PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+
     MONGO_URI: process.env.MONGO_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET
+    
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
 };
 
 module.exports = ENV;
