95ccb087408150632210c53d4767d44c52783ae8
Gnel <gshahnazaryan08@gmail.com>
2026-07-07 17:19:59 +0400

    chore: add security middlewares

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 const express = require("express");
+
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const errorHandler = require("./middlewares/error.middleware.js");
 const notFound = require("./middlewares/notFound.middleware.js");
 
+const routes = require("./routes");
+
 const app = express();
+
+app.use(helmet());
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
-const routes = require("./routes");
 app.use("/api", routes);
 
 app.use(notFound);
